7af12a13fef96909a0d06b10791816aaa3d3f078
jowala <owalajoab@gmail.com>
2024-07-30 07:16:08 +0300

    chore:login page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Login from "./Login";
+// import ManufacturerDashboard from './components/ManufacturerDashboard';
+// import DistributorDashboard from './components/DistributorDashboard';
+// import HealthFacilityDashboard from './components/HealthFacilityDashboard';
 
 function App() {
-  return <></>;
+  return (
+    <>
+    <Login />
+    {/* <Switch>
+      <Route path="/login"
+       
+         
+      </Route>
+    </Switch> */}
+    
+          </>
+  );
 }
 
 export default App;
